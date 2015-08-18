6d65d104f1513f93523cb5ab543d7c5e455957d9
tabchas <tabchas@gmail.com>
2015-08-18 17:37:03 -0500

    arduino code

---
+++ arduino/React-IR0/React-IR0.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+int prev = 0;
+
+void loop() {
+  if ((analogRead(0) > 270) != prev)
+  {
+    prev = (prev + 1) % 2;
+    Serial.print("{IR0");
+    Serial.print(prev);
+    Serial.print("}");
+  }
+}
